1624900f3fc7964844f131bc6aa2bc1acd148769
mesecg <mesecg@ttu.ee>
2021-02-06 18:11:33 +0300

    Empty lines cleaned

--- Vigenere/vigenere.py
+++ Vigenere/vigenere.py
@@ -1,4 +1,3 @@
-
 msg=input('Please write the message you want to encrypt/decrypt: ')
 key = input('Please give the key: ')
 choise = input('Please write 0 to Encrypt and 1 to Decrypt: ')
@@ -6,12 +5,10 @@ def cipher():
     count = 0
     if(choise =="0"):
         for x in msg:
-    
             print(chr(65 + ((ord(x.upper()))+ (ord((key[count%len(key)]).upper())))%26), end ="")
             count+=1
     elif (choise =="1"):
         for x in msg:
-    
             print(chr(65 + ((ord(x.upper()))- (ord((key[count%len(key)]).upper())))%26), end ="")
             count+=1
 cipher()

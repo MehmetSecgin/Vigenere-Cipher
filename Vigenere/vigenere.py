99c2d27d5da872d232a672d0fbaf79011e43c772
mesecg <mesecg@ttu.ee>
2021-02-06 18:28:42 +0300

    added spaces

--- Vigenere/vigenere.py
+++ Vigenere/vigenere.py
@@ -1,14 +1,20 @@
-msg=input('Please write the message you want to encrypt/decrypt: ')
-key = input('Please give the key: ')
-choise = input('Please write 0 to Encrypt and 1 to Decrypt: ')
 def cipher():
+    msg=input('Please write the message you want to encrypt/decrypt: ')
+    key = input('Please give the key: ')
+    choise = input('Please write 0 to Encrypt and 1 to Decrypt: ')
     count = 0
     if(choise =="0"):
         for x in msg:
-            print(chr(65 + ((ord(x.upper()))+ (ord((key[count%len(key)]).upper())))%26), end ="")
-            count+=1
+            if(x != " "):
+                print(chr(65 + ((ord(x.upper()))+ (ord((key[count%len(key)]).upper())))%26), end ="")
+                count+=1
+            else:
+                print(" ",end="")
     elif (choise =="1"):
         for x in msg:
-            print(chr(65 + ((ord(x.upper()))- (ord((key[count%len(key)]).upper())))%26), end ="")
-            count+=1
+            if(x !=" "):
+                print(chr(65 + ((ord(x.upper()))- (ord((key[count%len(key)]).upper())))%26), end ="")
+                count+=1
+            else:
+                print(" ",end="")
 cipher()
